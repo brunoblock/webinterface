f82ec4a6bdfd21ce3c67eb64a629450900693d47
Rebel Fornea <rebelfornea@gmail.com>
2018-09-25 10:08:09 -0400

    make component dumb

--- src/components/brokers-down/index.tsx
+++ src/components/brokers-down/index.tsx
@@ -1,15 +1,9 @@
 import React from "react";
-import { connect } from "react-redux";
 
-import BrokersDownSlide from ".//brokers-down-slide";
+import BrokersDownSlide from "./brokers-down-slide";
 
-const mapStateToProps = state => ({
-    history: state.upload.history
-});
-const mapDispatchToProps = dispatch => ({});
-
-const BrokersDownPage = ({ history }) => {
+const BrokersDownPage = () => {
     return <BrokersDownSlide/>;
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(BrokersDownPage);
+export default BrokersDownPage;
