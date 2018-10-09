3e1060adb690108a12e6a2c9610720447d97f732
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-10-09 10:23:25 -0400

    Better UI for clipboard button

--- src/components/shared/clipboard-button.tsx
+++ src/components/shared/clipboard-button.tsx
@@ -1,10 +1,34 @@
 import React from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
-const ClipboardBtn = ({ text, children }) => (
-  <CopyToClipboard text={text}>
-    <button className="clipboard-button">{children}</button>
-  </CopyToClipboard>
-);
+const POPUP_TIME = 300;
+
+interface ClipboardBtnProps {
+  text;
+}
+
+class ClipboardBtn extends React.Component<ClipboardBtnProps> {
+  state = { popTimeout: undefined };
+
+  render() {
+    return (
+      <CopyToClipboard
+        text={this.props.text}
+        onCopy={() => {
+          window.clearTimeout(this.state.popTimeout);
+          const popTimeout = window.setTimeout(
+            () => this.setState({ popTimeout: undefined }),
+            POPUP_TIME
+          );
+          this.setState({ popTimeout });
+        }}
+      >
+        <button className="clipboard-button">
+          {!!this.state.popTimeout ? "Copied!" : this.props.children}
+        </button>
+      </CopyToClipboard>
+    );
+  }
+}
 
 export default ClipboardBtn;
